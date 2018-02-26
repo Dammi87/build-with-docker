879af85d13e36e4c377bed53a53a1aaf837c7bc5
Unknown <87dammi@gmail.com>
2018-02-26 08:57:29 +0100

    Add messages to custom exceptions

--- src/custom_exceptions.py
+++ src/custom_exceptions.py
@@ -1,18 +1,33 @@
 class DockerFolderMissing(Exception):
-    pass
+    def __init__(self, message=None):
+        if message is None:
+            msg = "Make sure you have a folder called Dockerfiles in the root of the project"
+            Exception.__init__(msg)
 
 
 class DockerFilesMissing(Exception):
-    pass
+    def __init__(self, message=None):
+        if message is None:
+            msg = "Could not find a .Dockerfile in the specified folder"
+            Exception.__init__(msg)
 
 
 class FileMissing(Exception):
-    pass
+    def __init__(self, message=None):
+        if message is None:
+            msg = "Missing file"
+            Exception.__init__(msg)
 
 
 class ExecutionScriptMissing(Exception):
-    pass
+    def __init__(self, message=None):
+        if message is None:
+            msg = "You need to specify what script to execute"
+            Exception.__init__(msg)
 
 
 class NoDockerfileSpecified(Exception):
-    pass
+    def __init__(self, message=None):
+        if message is None:
+            msg = "You need to specify which Dockerfile to use as image"
+            Exception.__init__(msg)
